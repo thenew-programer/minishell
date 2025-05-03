3f6490bf7ef8e0379c515b2bddf7e6ac2776ac94
Youssef Bouryal <youssefbouryal02@gmail.com>
2025-05-03 11:53:51 +0100

    add ft_strndup to use instead of the standard strndup because 42 wants to make our life harder now to be confortable later

--- libft/ft_strndup.c
+++ libft/ft_strndup.c
@@ -1,36 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_strndup.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ybouryal <ybouryal@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/22 17:55:59 by ybouryal          #+#    #+#             */
-/*   Updated: 2025/05/03 08:59:54 by ybouryal         ###   ########.fr       */
+/*   Created: 2025/05/03 08:56:40 by ybouryal          #+#    #+#             */
+/*   Updated: 2025/05/03 09:00:48 by ybouryal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+char	*ft_strndup(const char *s, size_t size)
 {
-	size_t	i;
-	size_t	len;
-	size_t	src_len;
+	char	*str;
 
-	src_len = ft_strlen(src);
-	if (size == 0)
-		return (src_len);
-	if (src_len + 1 < size)
-		len = src_len;
-	else
-		len = size - 1;
-	i = 0;
-	while (i < len)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[len] = 0;
-	return (src_len);
+	str = malloc(sizeof(char) * (size + 1));
+	if (!str)
+		return (NULL);
+	ft_strlcpy(str, s, size + 1);
+	return (str);
 }
